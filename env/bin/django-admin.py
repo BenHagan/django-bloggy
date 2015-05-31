337dc7ed22eee0758b8cc1b6f346ef62f6752996
chilltrain <chilltrain@users.noreply.github.com>
2015-05-31 16:02:30 -0500

    initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ben/Documents/Dev/django-quick-start/django-bloggy/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
