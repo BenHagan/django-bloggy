46177b2d549582d5988a9c8b8d13da4f5c1ce8e8
textheadmin <textheadmin@users.noreply.github.com>
2015-06-01 01:01:30 -0500

    friendly URLs

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -8,11 +8,13 @@ from blog.models import Post
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
     template = loader.get_template('blog/index.html')
+    for post in latest_posts:
+        post.url = post.title.replace(' ',  '_')
     html = template.render({'latest_posts': latest_posts})
     return HttpResponse(html)
 
-def post(request, post_id):
-    single_post = get_object_or_404(Post, pk=post_id)
+def post(request, post_url):
+    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
     template = loader.get_template('blog/post.html')
     html = template.render({'single_post': single_post})
     return HttpResponse(html)
